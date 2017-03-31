d99d3c8a5c6a5009f0ead0e3c244169c0250a58d
Petar Stankov <stankovpetar04@gmail.com>
2017-03-31 10:51:23 +0200

    login design

--- PoslovnaBanka/app/controllers/PocetnaStranica.java
+++ PoslovnaBanka/app/controllers/PocetnaStranica.java
@@ -1,10 +1,12 @@
 package controllers;
 
 import play.mvc.Controller;
+import play.mvc.With;
 
 /**
  * Created by Djordje on 3/26/2017.
  */
+@With(Secure.class)
 public class PocetnaStranica extends Controller {
 
     public static void show(String mode){
