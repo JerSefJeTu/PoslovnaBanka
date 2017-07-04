d7a1d1314ba28dc92c5267563ca705aa7bc2b648
Djordje Nedovic <nedovic.djordje@gmail.com>
2017-07-03 18:05:52 -0700

    ispravljene sitne greske i manji problem sa gitom, funkcionalnost za izvestaje bi trebala da bude potpuna

--- PoslovnaBanka/app/controllers/Izvestaji.java
+++ PoslovnaBanka/app/controllers/Izvestaji.java
@@ -22,6 +22,10 @@ import java.util.Map;
  */
 public class Izvestaji extends Controller {
 
+    public static final String dirPath = "./izvestaji/";
+    public static final String dbUrl = "jdbc:mysql://localhost:3306/poslovna";
+    public static final String user = "root";
+    public static final String passowrd = "1234";
     public static void show()
     {
         render();
@@ -38,11 +42,11 @@ public class Izvestaji extends Controller {
             params.put("username", username);
             if(klijent instanceof FizickoLice){
                 System.out.println("fizicko");
-                JasperPrint jp = JasperFillManager.fillReport( "D://Poslovna Informatika Banka/PoslovnaBanka/PoslovnaBanka/izvestaji/report1Fizicko.jasper",params,DriverManager.getConnection("jdbc:mysql://localhost:3306/poslovna2", "root", "1234") );
+                JasperPrint jp = JasperFillManager.fillReport( dirPath+"IzvodFizickoLice.jasper",params,DriverManager.getConnection(dbUrl, user, passowrd) );
                 JasperViewer.viewReport(jp, false);
             }else{
                 System.out.println("pravno");
-                JasperPrint jp = JasperFillManager.fillReport( "D://Poslovna Informatika Banka/PoslovnaBanka/PoslovnaBanka/izvestaji/report1Pravno.jasper",params,DriverManager.getConnection("jdbc:mysql://localhost:3306/poslovna2", "root", "1234") );
+                JasperPrint jp = JasperFillManager.fillReport( dirPath+"IzvodPravnoLice.jasper",params,DriverManager.getConnection(dbUrl, user, passowrd));
                 JasperViewer.viewReport(jp, false);
             }
 
@@ -56,7 +60,7 @@ public class Izvestaji extends Controller {
         try {
             Map params = new HashMap(1);
             params.put("swiftKod", swiftKod);
-            JasperPrint jp = JasperFillManager.fillReport( "D://Poslovna Informatika Banka/PoslovnaBanka/PoslovnaBanka/izvestaji/report2.jasper",params,DriverManager.getConnection("jdbc:mysql://localhost:3306/poslovna2", "root", "1234") );
+            JasperPrint jp = JasperFillManager.fillReport( dirPath+"IzvestajRacunaZaZadatuBanku.jasper",params,DriverManager.getConnection(dbUrl, user, passowrd));
             JasperViewer.viewReport(jp, false);
         } catch (Exception ex) {
             ex.printStackTrace();
